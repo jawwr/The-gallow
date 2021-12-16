029b31d0c974a95293fa129322b0dbc809e52040
Arseny <20mineslend14@gmail.com>
2021-12-16 16:08:56 +0500

    виселица

---
+++ the gallows/Gallows.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace the_gallows
+{
+    public class Gallows
+    {
+        public static void Input(string word, int health)
+        {
+            DrawWords(word, new List<char>(),health);
+            char input;
+            List<char> letter = new List<char>();
+            bool end = false;
+            do
+            {
+                input = char.Parse(Console.ReadLine());
+                if (word.Contains(input, StringComparison.OrdinalIgnoreCase))
+                    letter.Add(input);
+                else
+                    health--;
+                
+                DrawWords(word, letter, health);
+                if (health == 0) 
+                { 
+                    Console.WriteLine("Вы проиграли");
+                    break;
+                }
+            } while (!end);
+        }
+
+        static void DrawWords(string word,List<char> letters, int health)
+        {
+            Console.Clear();
+            string newword = string.Empty;
+            for (int i = 0; i < word.Length; i++)
+            {
+                newword += letters.Contains(word[i])
+                    ? word[i]
+                    : '_';
+            }
+            Console.WriteLine(newword);
+            Console.WriteLine($"Жизни:{health}");
+        }
+    }
+}
---
+++ the gallows/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace the_gallows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = GetWords();
+            int health = 8;
+            Gallows.Input(word,health);
+        }
+
+        static string GetWords()
+        {
+            string[] words = File.ReadAllLines("words.txt");
+            return words[GetWordNumber(words.Length - 1)];
+        }
+
+        static int GetWordNumber(int max)
+        {
+            Random random = new Random();
+            return random.Next(0, max);
+        }
+    }
+}
